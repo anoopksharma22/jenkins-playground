5bb02b4d8e33481bc9d7ceec738dcc03966f5e39
Anoop Sharma <anoop@192.168.1.17>
2025-08-29 23:59:17 +0530

    initial commit

---
+++ src/main/java/in/itsanoop/jenkins_playground/JenkinsPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package in.itsanoop.jenkins_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/itsanoop/jenkins_playground/JenkinsPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package in.itsanoop.jenkins_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
